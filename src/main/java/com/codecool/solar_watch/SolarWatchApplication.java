62102d1709a34472f3a75984379f74277c4256e2
mmarci96 <sarosdimarci@gmail.com>
2025-06-04 09:48:39 +0200

    initial spring boot app

---
+++ src/main/java/com/codecool/solar_watch/SolarWatchApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.solar_watch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarWatchApplication.class, args);
+	}
+
+}
